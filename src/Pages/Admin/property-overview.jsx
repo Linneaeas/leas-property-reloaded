49f826c002b0456c36868bb5c20a32842f40fafd
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-03 14:18:02 +0200

    Detailed description of calcultations

--- src/Pages/Admin/property-overview.jsx
+++ src/Pages/Admin/property-overview.jsx
@@ -72,12 +72,13 @@ export function AdminPropertyOverview() {
     const suites = JSON.parse(localStorage.getItem("suites"));
     //Definierar suites i den här komponenten för att kunna hantera, lägga till och räkna ut data baserat på suiten, då  det är utifrån suiten jag visar datan.
     const standards = getStandardsFromLocalStorage() || [];
+    //OM det finns standards, blir dem assigned const "standards", finns inte, kommer en tom array tilldelas "standards"(error prev.).
     const roomtypes = getRoomtypesFromLocalStorage() || [];
     const facilities = getFacilitiesFromLocalStorage() || [];
-    const suiteCapacities = calculateSuiteCapacity(standards, roomtypes, beds);
+    const suiteCapacities = calculateSuiteCapacity(standards, roomtypes, beds); //Funktionen skapas och tar standards..etc som argument.
 
     if (suites && suites.length > 0 && standards && standards.length > 0) {
-      //Kollar så suites och standards har ett värden=True, (null eller undefiened=False)
+      //Kollar så suites och standards har ett värden=True, (null eller undefiened=False)(error prev.)
       return suites.map((suite) => {
         //OM ovan värde är true:
         const standard = standards.find(
@@ -86,7 +87,7 @@ export function AdminPropertyOverview() {
         if (standard) {
           //Om en matchande standard hittades:
           const roomtypeOptions = standard.roomtypeOptions || {};
-          //Sätter ihop "roomtypeOptions" utifrån "standard"". || Om en standard saknar "roomtypeOptions", är standardens roomtypeOption ett tomt objekt.
+          //Sätter ihop "roomtypeOptions" utifrån "standard"". ||OR Om en standard saknar "roomtypeOptions", är standardens roomtypeOption ett tomt objekt.
           const facilitieOptions = standard.facilitieOptions || {};
           return {
             ...suite,
@@ -104,39 +105,44 @@ export function AdminPropertyOverview() {
   function calculateRoomtypeCapacity(roomtype, beds) {
     //Funktion med 2 parametrar.
     return Object.keys(roomtype.bedOptions).reduce((totalCapacity, bedId) => {
-      //Tar emot en array med "Object"s "keys"(bed ID) utifrån vilket "bedOptions" rumstypen har. Den tar fram sängens ID som är accossierad med den rumstypen.
-      //Använder "REDUCE" metoden för att gå igenom arrayen med bedId:s. Den tar fram ett värde (totalCapacity) genom att applicera en funktion för varje element(bedID) i arrayen).
+      //Object.keys...: Tar emot en array med "Object"s "keys"(bed ID) utifrån vilket "bedOptions" rumstypen har. Den tar fram sängens ID som är accossierad med den rumstypen.
+      //Använder "REDUCE" metoden för att gå igenom arrayen med bedId:s (från Object.keys(roomtype.bedOptions)). Callback funktionen tar 2 parametrar. För varje bedId, kalkylerar funktionen & tar fram den totala kapaciteten baserat på antal personer i varje bed, och antalet beds i den rumstypen.
       const bed = beds.find((bed) => bed.id === parseInt(bedId));
-      //Söker igenom arrayen för att hitta(FIND) bed Objektet som matchat bedId:et, genom parseInt säkerhetställs att bedId  behandlas som en integer.
+      //Söker igenom arrayen för att hitta(FIND) bed Objektet som matchat bedId:et, genom parseInt säkerhetställs att bedId behandlas som en integer(ser till en korrekt jämförelse i beds arrayn)
       if (bed) {
         //OM en säng med matchande Id hittas, utför koden nedan:
         const bedCapacity = bed.selectedBedPersons || 0;
         const bedCount = roomtype.bedOptions[bedId] || 0;
         return totalCapacity + bedCapacity * bedCount;
-        //Räknar ut Kapaciteten för den specifika sängen och läggar till det i den totala kapaciteten som är uträknad än så länge.
+        //Räknar ut Kapaciteten för den specifika sängen och läggar till det i den totala kapaciteten som är uträknad än så länge.(OR "0" error prev.)
       }
       return totalCapacity; //
-    }, 0); //OM en säng med matchande id Inte hittas, returneras den nuvarande totalCapacity utan några forändringar.
+    }, 0); //OM en säng med matchande id Inte hittas, returneras den nuvarande totalCapacity utan några förändringar.("0" är det initiala värdet som skickas som argument till Reduce metoden)
   }
 
   function calculateSuiteCapacity(standards, roomtypes, beds) {
+    //Funktion med 3 parametrar.
     return standards.map((standard) => {
+      //Map varje standard i standards arrayn, för varje standard=ändra om till ett nytt objekt med en tillagd egenskap: "capacity".
       const capacity = roomtypes
-        .filter((roomtype) => roomtype.standardId === standard.id)
+        .filter((roomtype) => roomtype.standardId === standard.id) //För varje standard, filtrerar roomtypes och väljer endast dem som matchar med standardId.
         .reduce((totalCapacity, roomtype) => {
-          return totalCapacity + calculateRoomtypeCapacity(roomtype, beds);
+          //Reduce går igenom den filtrerade roomtypes för den standarden. För varje roomtype:...
+          return totalCapacity + calculateRoomtypeCapacity(roomtype, beds); //...räknar den ut den totala kapaciteten genom calculateRoomtypeCapacity funktionen och tar fram the "totalCapacity"
         }, 0);
-      return { ...standard, capacity };
+      return { ...standard, capacity }; //För varje standard returneras ett nytt "standard" objekt med dem orginala egenskaperna+den tillagda egenskapen "capacity", som representerar totala kapaciteten för dem associerade "roomtypes" baserat på sängarna.
     });
   }
 
   function calculateTotalPersonsInSuite(suite, roomtypes, beds) {
-    let totalPersons = 0;
+    let totalPersons = 0; //Initialt värde for att ha koll på totala antalet personer i Varje suite.
     if (suite && suite.selectedRoomtypes && beds) {
+      //Kollar så alla är definerade och sannna innan det räknas på.
       totalPersons = roomtypes.reduce((totalPersons, roomtype) => {
+        //Använder Reduce for att räkna ut tot antalet personer i suiten baserat på varje roomtype och dens associerade roomCount & bed capacity:
         const roomCount = suite.selectedRoomtypes[roomtype.id] || 0;
         const bedCapacity = calculateRoomtypeCapacity(roomtype, beds);
-        totalPersons += roomCount * bedCapacity;
+        totalPersons += roomCount * bedCapacity; //För varje roomtype, räknas antalet personer i den rumstypen ut och totala antalet personer för varje suite tas fram.
         return totalPersons;
       }, 0);
     }
@@ -146,12 +152,14 @@ export function AdminPropertyOverview() {
   const calculateTotalRoomsInSuite = (suite) => {
     let totalRooms = 0;
     if (suite && suite.selectedRoomtypes) {
+      //Kollar så alla är definerade och sannna innan det räknas på.
       for (const roomtype of roomtypes) {
-        const roomCount = suite.selectedRoomtypes[roomtype.id] || 0;
-        totalRooms += parseInt(roomCount, 10);
+        //Går igenom varje roomtype i den globala roomtypes arrayn.
+        const roomCount = suite.selectedRoomtypes[roomtype.id] || 0; //För varje roomtype tas roomCount fram från suite.selectedRoomtypes fram, baserat på roomtype.id
+        totalRooms += parseInt(roomCount, 10); //Tar fram totala antalet rum. ("10" spec base radix, 10=decimal nummer. error prev.)
       }
     }
-    return totalRooms;
+    return totalRooms; //Totala rumsantalet per Suite.
   };
 
   return (
